79fecbf0335e61fede144b78ceb0f2c322f3804b
miyatamsd <Masahide.Miyata.bp@jp.nttdata.com>
2026-03-25 16:30:27 +0900

    更新

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const distDir = appEnv === 'production' ? '.next' : `.next-${appEnv}`;
 
 module.exports = {
   reactStrictMode: true,
-  // ビルドコマンドで渡した APP_ENV をそのまま出力先名に反映
-  distDir: `.next-${appEnv}`,
+  // production は Vercel 互換の .next、それ以外は環境名付き
+  distDir,
   // env ファイルの REMOVE_CONSOLE=true/false で制御
   compiler: {
     removeConsole: process.env.REMOVE_CONSOLE === 'true',

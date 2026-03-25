78a23a61556322a377b77329e42759dea61a7896
miyatamsd <Masahide.Miyata.bp@jp.nttdata.com>
2026-03-25 16:16:24 +0900

    更新

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 /** @type {import('next').NextConfig} */
 
 const appEnv = process.env.APP_ENV || 'default';
+const distDir = appEnv === 'production' ? '.next' : `.next-${appEnv}`;
 
 module.exports = {
   reactStrictMode: true,
@@ -10,6 +11,10 @@ module.exports = {
   // env ファイルの REMOVE_CONSOLE=true/false で制御
   compiler: {
     removeConsole: process.env.REMOVE_CONSOLE === 'true',
-  }
+  },
+  env: {
+    APP_ENV: process.env.APP_ENV,
+  },
+  // 必要に応じて他の設定を追加してください
 };
 
